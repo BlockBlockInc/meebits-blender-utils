b6bc578a6a63c1708ec5d24277c2c3b2c0eb5d4c
Josh Peters <joshpeters52@gmail.com>
2021-07-06 14:09:03 -0700

    Add queue

--- docker/server/app.js
+++ docker/server/app.js
@@ -8,13 +8,18 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as util from 'util';
 import * as child_process from 'child_process'
+import Queue from 'bee-queue'
+import { doesNotMatch } from 'assert';
 
+// ---------------- Exec Setup ----------------
+const exec = util.promisify(child_process.exec);
+
+// ---------------- Express Setup ----------------
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const exec = util.promisify(child_process.exec);
-
+// ---------------- Google Setup ----------------
 const auth = new google.auth.GoogleAuth({
   keyFile: `${path.resolve()}/google-service-account.json`,
   scopes: ['https://www.googleapis.com/auth/drive.file'],
@@ -25,56 +30,105 @@ const service = google.drive({
   auth, auth
 });
 
-app.get('/', async (req, res) => {
-  res.send({ status: "healthy" })
+// ---------------- Queue Setup ----------------
+const options = {
+  removeOnSuccess: true,
+}
+const conversionQueue = new Queue('conversion', options);
+const uploadQueue = new Queue('upload', options);
+conversionQueue.on('ready', () => {
+  console.log('Conversion queue ready');
+});
+conversionQueue.on('ready', () => {
+  console.log('Upload queue ready');
 });
 
-// Enable logging after healthcheck route so logs don't get polluted
-app.use(morgan('tiny'));
+function addConversionJob(meebit) {
+  return conversionQueue
+    .createJob(meebit)
+    .retries(3)
+    .save();
+}
 
-app.post('/webhook', async (req, res) => {
+function addUploadJob(meebit_converted) {
+  return uploadQueue
+    .createJob(meebit_converted)
+    .retries(5)
 
-  const { fileUrl, folderId } = req.body;
-  const meebitFileName = fileUrl.split('/').pop();
-  console.log(`Downloading file: ${meebitFileName}`)
+    .save();
+}
+
+conversionQueue.process(async (job) => {
+  const { fileUrl, folderId, meebitFileName } = job.data;
+  const meebitBaseFileName = meebitFileName.split('.')[0];
+  const extensions = ['.fbx', '.glb', '.jpg', '.mtl', '.obj', '.vrm'];
+  console.log(`Conversion Queue starting: job ${job.id}, file ${meebitFileName}, folder ${folderId}`);
+  // Download
   try {
     const destination = fs.createWriteStream(`./meebits/${meebitFileName}`);
     const response = await fetch(fileUrl)
     response.body.pipe(destination);
   } catch (e) {
     const errMessage = `Error downloading file ${meebitFileName}`;
-    console.error(errMessage)
-    console.error(e);
-    res.status(500).send({ message: errMessage });
-    return;
+    console.log(errMessage)
+    console.log(e);
+    return Promise.reject(new Error(errMessage));
   }
+
+  // Convert
   try {
-    console.log(`Running conversion for file: ${meebitFileName}`)
     await exec('./convert_all_meebits.sh');
-    await exec(`rm ./meebits/${meebitFileName}`);
+    extensions.map(extension => {
+      // Check if all files exist, if not convert script failed.
+      const convertedFileName = `${meebitBaseFileName}${extension}`;
+      if (!fs.existsSync(`./output_vrm/${convertedFileName}`)) {
+        const errMessage = `Error with conversion script, file ${meebitFileName}`;
+        console.log(errMessage)
+        console.log(e);
+        return Promise.reject(new Error(errMessage));
+      }
+    });
   } catch (e) {
     const errMessage = `Error converting file ${meebitFileName}`;
-    console.error(errMessage)
-    console.error(e);
-    res.status(500).send({ message: errMessage });
-    return;
+    console.log(errMessage)
+    console.log(e);
+    await exec(`rm ./output_vrm/${meebitBaseFileName}.*`);
+    return Promise.reject(new Error(errMessage));
+  } finally {
+    await exec(`rm ./meebits/${meebitFileName}`);
   }
 
-  res.send({ file: meebitFileName });
+  // Add to upload queue
+  try {
+    extensions.map(extension => {
+      const convertedFileName = `${meebitBaseFileName}${extension}`;
+      addUploadJob({
+        folderId, convertedFileName
+      });
+    });
+  } catch (e) {
+    const errMessage = `Error adding to upload queue file ${meebitFileName}`;
+    console.log(errMessage)
+    console.log(e);
+    return Promise.reject(new Error(errMessage));
+  }
 
-  const meebitBaseFileName = meebitFileName.split('.')[0];
-  const extensions = ['.fbx', '.glb', '.jpg', '.mtl', '.obj', '.vrm'];
+  console.log(`Conversion Queue done: job ${job.id}, file ${meebitFileName}, folder ${folderId}`);
+  return { status: "done" }
 
-  for (let i = 0; i < extensions.length; i++) {
-    const convertedFileName = `${meebitBaseFileName}${extensions[i]}`;
-    const convertedFilePath = `./output_vrm/${convertedFileName}`
+});
 
-    if (!fs.existsSync(convertedFilePath)) {
-      console.error(`File not found: ${convertedFileName} - not uploading`);
-      break;
-    }
+addQueue.process(async (job) => {
+  console.log(`Processing job ${job.id}`);
+  return job.data.x + job.data.y;
+});
 
-    console.log(`Starting upload: ${convertedFileName}`)
+uploadQueue.process(async (job) => {
+  const { folderId, convertedFileName } = job.data;
+  const convertedFilePath = `./output_vrm/${convertedFileName}`
+  console.log(`Upload Queue starting: job ${job.id}, file ${convertedFileName}, folder ${folderId}`);
+
+  try {
     await service.files.create({
       requestBody: {
         name: convertedFileName,
@@ -83,16 +137,38 @@ app.post('/webhook', async (req, res) => {
       media: {
         body: fs.createReadStream(convertedFilePath),
       }
-    }).catch(e => {
-      console.error(`Error uploading ${convertedFileName}`)
-      console.error(e);
-      res.status(500).send(e.message);
-      return;
     });
-    await exec(`rm ./output_vrm/${convertedFileName}`);
-    console.log(`Done upload: ${convertedFileName}`)
+  } catch (e) {
+    const errMessage = `Error uploading file ${convertedFileName}`;
+    console.log(errMessage)
+    console.log(e);
+    return Promise.reject(new Error(errMessage));
   }
-  console.log(`Done uploading file: ${meebitFileName}`)
+  await exec(`rm ${convertedFilePath}`);
+  console.log(`Upload Queue done: job ${job.id}, file ${convertedFileName}, folder ${folderId}`);
+  return { status: "done" }
+
 });
 
+// ---------------- Express Routes ----------------
+
+app.get('/', async (req, res) => {
+  res.send({ status: "healthy" });
+});
+
+// Enable logging after healthcheck route so logs don't get polluted
+app.use(morgan('tiny'));
+
+app.post('/webhook', async (req, res) => {
+  const { fileUrl, folderId } = req.body;
+  const meebitFileName = fileUrl.split('/').pop();
+  try {
+    addConversionJob({ fileUrl, folderId, meebitFileName })
+  } catch {
+    res.status(500).send({ error: "Something went wrong, check the logs and try again" });
+  }
+  res.send({ status: "processing" });
+});
+
+// ---------------- Start Server ----------------
 app.listen(process.env.PORT || 3000, () => console.log(`🚀 Server ready`));
---
+++ docker/server/conversion-script.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+
+async function main() {
+
+  const data = [{
+    "fileUrl": "",
+    "folderId": ""
+  }]
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
